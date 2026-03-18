6704a9f6af21a22ffad95a85cee88fa7eb03ad5a
Lucas Fagundes <lucasfagundesice@gmail.com>
2026-03-18 09:54:09 -0300

    feat: implementa topo da landing page com curva, gradiente e layout responsivo

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,65 +1,147 @@
-import Image from "next/image";
+"use client";
+import {
+    HStack,
+    Image as ChakraImage,
+    Box,
+    SelectItemGroup,
+    Select,
+    Portal,
+    createListCollection,
+    Button,
+    Text,
+    VStack,
+    Flex,
+    Heading,
+    Input,
+} from "@chakra-ui/react";
+import NextImage from "next/image";
+import netflixLogo from "../../public/assets/netflixLogo.svg";
+import caretDown from "../../public/assets/caretDown.svg";
+import { LuLanguages } from "react-icons/lu";
+import Header from "@/components/Header/header";
+import netflixBackground from "../../public/assets/netflix-background.jpg";
+import { Open_Sans } from "next/font/google";
+
+const idiomas = createListCollection({
+    items: [
+        { label: "Português", value: "pt" },
+        { label: "English", value: "en" },
+    ],
+});
 
 export default function Home() {
-  return (
-    <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={100}
-          height={20}
-          priority
-        />
-        <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
-          <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
-            To get started, edit the page.tsx file.
-          </h1>
-          <p className="max-w-md text-lg leading-8 text-zinc-600 dark:text-zinc-400">
-            Looking for a starting point or more instructions? Head over to{" "}
-            <a
-              href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
+    return (
+        <Box position="relative" w="100%" h="100vh" overflow="hidden">
+            <Box
+                position="absolute"
+                inset={0}
+                zIndex={0}
             >
-              Templates
-            </a>{" "}
-            or the{" "}
-            <a
-              href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Learning
-            </a>{" "}
-            center.
-          </p>
-        </div>
-        <div className="flex flex-col gap-4 text-base font-medium sm:flex-row">
-          <a
-            className="flex h-12 w-full items-center justify-center gap-2 rounded-full bg-foreground px-5 text-background transition-colors hover:bg-[#383838] dark:hover:bg-[#ccc] md:w-[158px]"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={16}
-              height={16}
-            />
-            Deploy Now
-          </a>
-          <a
-            className="flex h-12 w-full items-center justify-center rounded-full border border-solid border-black/[.08] px-5 transition-colors hover:border-transparent hover:bg-black/[.04] dark:border-white/[.145] dark:hover:bg-[#1a1a1a] md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Documentation
-          </a>
-        </div>
-      </main>
-    </div>
-  );
+                <NextImage
+                    src={netflixBackground}
+                    alt="plano de fundo da netflix"
+                    fill
+                    style={{ objectFit: "cover" }}
+                    priority
+                />
+
+                <Flex
+                    zIndex={2}
+                    position={"absolute"}
+                    w="100%"
+                    h="100%"
+                    bgColor="blackAlpha.700/70"
+                />
+                <Box
+                    position="absolute"
+                    inset={0}
+                    zIndex={1}
+                    bg={`
+        radial-gradient(100% 130% at 50% 150%, rgba(0,0,0,0.7) 70%, rgba(0,0,0,0.3) 100%)
+    `}
+                />
+                <Box
+                    position="absolute"
+                    bottom={0}
+                    left="50%"
+                    w="115%"
+                    transform="translateX(-50%)"
+                    h="85px"
+                    zIndex={3}
+                    borderTopLeftRadius="50% 100%"
+                    borderTopRightRadius="50% 100%"
+                    bg={`
+        radial-gradient(40% 100% at 50% 5%, rgba(64,97,231,0.35), transparent 90%),
+        black
+    `}
+                    bgClip="padding-box"
+                    overflow="hidden"
+                    _before={{
+                        content: '""',
+                        position: "absolute",
+                        top: 0,
+                        left: 0,
+                        right: 0,
+                        height: "100%",
+                        padding: "4px",
+                        background: `
+                            linear-gradient(to right,
+                                rgba(33,13,22,0) 0%,
+                                rgba(184,40,105,0.7) 15%,
+                                rgba(229,9,20,1) 50%,
+                                rgba(184,40,105,0.7) 85%,
+                                rgba(33,13,22,0) 100%
+                            )
+                            `,
+                        borderTopLeftRadius: "50% 100%",
+                        borderTopRightRadius: "50% 100%",
+                        clipPath: "inset(0 0 30% 0)",
+                        WebkitMask:
+                            "linear-gradient(#fff 0 0) content-box, linear-gradient(#fff 0 0)",
+                        WebkitMaskComposite: "xor",
+                        maskComposite: "exclude",
+                    }}
+                />
+            </Box>
+
+            <Header position="absolute" top={0} zIndex={50} />
+
+            <VStack marginX="500px" position="relative" zIndex={2} pt="345px" color="white">
+                <Heading
+                    fontSize={{ lg: "62px" }}
+                    fontWeight="black"
+                    lineHeight="80px"
+                    minW="588px"
+                    maxH="140px"
+                    marginBottom="32px"
+                    textAlign="center"
+                    textWrap="balance"
+                    fontFamily={"sans-serif"}
+                    letterSpacing={"tighter"}
+                >
+                    Filmes, séries e muito mais, sem limites
+                </Heading>
+                <Text fontSize="20px" fontWeight="500"marginBottom="28px" fontFamily={"sans-serif"}>
+                    A partir de R$ 20,90. Cancele quando quiser.
+                </Text>
+                <Text fontSize={"16px"} margin={0} fontFamily={"sans-serif"}>
+                    Quer assistir? Informe seu email para criar ou reiniciar sua assinatura.
+                </Text>
+                <HStack marginTop="8px">
+                    <Input
+                        placeholder="Email"
+                        fontWeight="700"
+                        padding="24px 16px 8px 16px"
+                        w="395px"
+                        h="56px"
+                        border="1px solid rgba(255, 255, 255, 0.3)"
+                        bg="gray.950/80"
+                    />
+                    <Button color="#fff"bgColor="red" h="56px" w="184px" fontSize="24px" fontWeight="700" fontFamily={"sans-serif"}>
+                        Vamos Lá
+                    </Button>
+                </HStack>
+            </VStack>
+        </Box>
+    );
 }
---
+++ src/components/Header/header.tsx
@@ -0,0 +1,127 @@
+"use client";
+import {
+    HStack,
+    Image as ChakraImage,
+    Box,
+    SelectItemGroup,
+    Select,
+    Portal,
+    createListCollection,
+    Button,
+    Flex,
+    StackProps,
+} from "@chakra-ui/react";
+import NextImage from "next/image";
+import netflixLogo from "../../../public/assets/netflixLogo.svg";
+import caretDown from "../../../public/assets/caretDown.svg";
+import { LuLanguages } from "react-icons/lu";
+import netflixBackground from "../../../public/assets/netflix-background.jpg";
+
+const idiomas = createListCollection({
+    items: [
+        { label: "Português", value: "pt" },
+        { label: "English", value: "en" },
+    ],
+});
+
+export default function Header(props: StackProps) {
+    return (
+        <HStack
+            zIndex={50}
+            px={{ base: "32px", md: "148px", lg: "338px" }}
+            py={0}
+            w="100vw"
+            h={120}
+            align="flex-start"
+            background="linear-gradient(180deg, rgba(0, 0, 0, 0.8) 0%, rgba(0, 0, 0, 0.7889) 8.333%, rgba(0, 0, 0, 0.7556) 16.67%, rgba(0, 0, 0, 0.7) 25%, rgba(0, 0, 0, 0.6222) 33.33%, rgba(0, 0, 0, 0.5222) 41.67%, rgba(0, 0, 0, 0.4) 50%, rgba(0, 0, 0, 0.2778) 58.33%, rgba(0, 0, 0, 0.1778) 66.67%, rgba(0, 0, 0, 0.1) 75%, rgba(0, 0, 0, 0.0444) 83.33%, rgba(0, 0, 0, 0.0111) 91.67%, transparent 100%)"
+            {...props}
+        >
+            <HStack
+                h={88}
+                w="100%"
+                flexDirection="row"
+                justify="space-between"
+                paddingRight="30px"
+            >
+                <Box w="30%">
+                    <NextImage
+                        height={40}
+                        width={148}
+                        src={netflixLogo}
+                        alt="Netflix"
+                    />
+                </Box>
+                <HStack w="100%" justify={"flex-end"} gap={0}>
+                    <Select.Root
+                        collection={idiomas}
+                        size="xs"
+                        width="146px"
+                        height="32px"
+                        defaultValue={["pt"]}
+                        fontWeight="black"
+                        fontFamily="sans-serif"
+                    >
+                        <Select.HiddenSelect />
+                        <Select.Control>
+                            <Select.Trigger
+                                bg="gray.950/80"
+                                color="white"
+                                border="1px solid rgba(255, 255, 255, 0.3)"
+                                borderRadius="4px"
+                                px={3}
+                                _open={{
+                                    borderStyle: "double",
+                                    borderColor: "#fff",
+                                }}
+                                display="flex"
+                                alignItems="center"
+                                fontSize={16}
+                                fontWeight="400"
+                            >
+                                <LuLanguages />
+
+                                <Select.ValueText placeholder="Idioma" />
+
+                                <NextImage
+                                    height={3.86}
+                                    width={7.53}
+                                    src={caretDown}
+                                    alt="Icone para abrir a caixa de seleção de idiomas"
+                                />
+                            </Select.Trigger>
+                        </Select.Control>
+
+                        <Portal>
+                            <Select.Positioner>
+                                <Select.Content
+                                    bg="#fff"
+                                    color="#000"
+                                    borderRadius="initial"
+                                    fontSize={16}
+                                >
+                                    {idiomas.items.map((item) => (
+                                        <Select.Item
+                                            textAlign={"center"}
+                                            item={item}
+                                            key={item.value}
+                                            _hover={{
+                                                bg: "blue.600",
+                                                color: "#fff",
+                                            }}
+                                            cursor="pointer"
+                                        >
+                                            {item.label}
+                                        </Select.Item>
+                                    ))}
+                                </Select.Content>
+                            </Select.Positioner>
+                        </Portal>
+                    </Select.Root>
+                    <Button ml="12px" h="32px" bgColor="rgb(229, 9, 20)" color="white" fontWeight="bold" fontSize={14} fontFamily="sans-serif">
+                        Entrar
+                    </Button>
+                </HStack>
+            </HStack>
+        </HStack>
+    );
+}
